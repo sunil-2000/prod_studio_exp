8575c7cc31a370b1c1062a2c5565640c973a0649
Courtney Beckham <cjb374@cornell.edu>
2022-11-28 11:16:20 -0500

    add ga tags

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from "./components/home";
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "UA-250162165-1";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
